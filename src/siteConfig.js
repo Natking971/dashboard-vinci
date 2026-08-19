5d9236965f1c3376c7f5254f3e59f53c07b1a96d
Natking971 <150266628+Natking971@users.noreply.github.com>
2026-08-19 16:03:09 +0200

    Create siteConfig.js
    Add multi-site configuration

---
+++ src/siteConfig.js
@@ -0,0 +1,84 @@
+export const SITE_CONFIGS = {
+  lpdl: {
+    id: "lpdl",
+    name: "La Poste du Louvre",
+    city: "Paris",
+
+    technicians: [
+      { id: 3, name: "Jason" },
+      { id: 2, name: "Cédric" },
+      { id: 1, name: "Ghulam" },
+    ],
+
+    tenants: [
+      "Parties communes",
+      "Voodoo",
+      "La Poste Enseigne",
+      "Logistique Urbaine",
+      "Louvre Banque Privée",
+      "IAD",
+    ],
+
+    sheets: {
+      planning:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpE027LVSx_f7HmnWQ3KbGXSYpp4dwuOqAcQMK-OLMn2zBxLH02mg7ckJFco6pr2rhYBbELNhCi9X8/pub?gid=0&single=true&output=csv",
+
+      affaires:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpE027LVSx_f7HmnWQ3KbGXSYpp4dwuOqAcQMK-OLMn2zBxLH02mg7ckJFco6pr2rhYBbELNhCi9X8/pub?gid=584135097&single=true&output=csv",
+
+      evenement:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpE027LVSx_f7HmnWQ3KbGXSYpp4dwuOqAcQMK-OLMn2zBxLH02mg7ckJFco6pr2rhYBbELNhCi9X8/pub?gid=1074854777&single=true&output=csv",
+
+      onesite:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpE027LVSx_f7HmnWQ3KbGXSYpp4dwuOqAcQMK-OLMn2zBxLH02mg7ckJFco6pr2rhYBbELNhCi9X8/pub?gid=1993330783&single=true&output=csv",
+
+      citations:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpE027LVSx_f7HmnWQ3KbGXSYpp4dwuOqAcQMK-OLMn2zBxLH02mg7ckJFco6pr2rhYBbELNhCi9X8/pub?gid=314910372&single=true&output=csv",
+    },
+  },
+
+  chateaudun: {
+    id: "chateaudun",
+    name: "44 Châteaudun",
+    city: "Paris",
+
+    technicians: [
+      { id: 1, name: "Michael" },
+      { id: 2, name: "Kamal" },
+    ],
+
+    tenants: [
+      "Parties communes",
+      "Paris Match",
+      "Commerzbank",
+      "Equativ",
+      "Snowflake",
+      "Checkout",
+    ],
+
+    sheets: {
+      planning:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQDih9gaVsrVuSLay_YrWHYG71kDsFuszy-qp1sixB5TIy7KfxuEwWD1NBMGdgbC2gOiKDI79H95x6b/pub?gid=0&single=true&output=csv",
+
+      affaires:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQDih9gaVsrVuSLay_YrWHYG71kDsFuszy-qp1sixB5TIy7KfxuEwWD1NBMGdgbC2gOiKDI79H95x6b/pub?gid=1474784177&single=true&output=csv",
+
+      evenement:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQDih9gaVsrVuSLay_YrWHYG71kDsFuszy-qp1sixB5TIy7KfxuEwWD1NBMGdgbC2gOiKDI79H95x6b/pub?gid=133639604&single=true&output=csv",
+
+      onesite:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQDih9gaVsrVuSLay_YrWHYG71kDsFuszy-qp1sixB5TIy7KfxuEwWD1NBMGdgbC2gOiKDI79H95x6b/pub?gid=1732473142&single=true&output=csv",
+
+      citations:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQDih9gaVsrVuSLay_YrWHYG71kDsFuszy-qp1sixB5TIy7KfxuEwWD1NBMGdgbC2gOiKDI79H95x6b/pub?gid=1813783748&single=true&output=csv",
+    },
+  },
+};
+
+export function getSiteConfig() {
+  const site =
+    import.meta.env.VITE_SITE ||
+    "lpdl";
+
+  return SITE_CONFIGS[site] || SITE_CONFIGS.lpdl;
+}
